b0817709607496a3f37f976c8a2225220643128e
CL <72822672+charleslippens@users.noreply.github.com>
2022-03-24 10:28:23 +0100

    Modification index.jsx routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+import "./style.css";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<Header />
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+			</Routes>
+		</Router>
+		<Footer />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
