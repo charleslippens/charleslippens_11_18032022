ff98b1d9cbc06bb60e280e091ae13144fc380bcd
CL <72822672+charleslippens@users.noreply.github.com>
2022-03-31 10:28:39 +0200

    modif page housing

--- src/Pages/housing.jsx
+++ src/Pages/housing.jsx
@@ -6,6 +6,7 @@ import housing from "../Data/logements.json";
 import Gallery from "../Components/gallery.jsx";
 import Tags from "../Components/tags.jsx";
 import Collapse from "../Components/collapse.jsx";
+import Rating from "../Components/rating.jsx";
 
 import "../Styling/housing.css";
 
@@ -14,8 +15,10 @@ function Housing() {
 	let params = useParams();
 	//Recherche de l'annonce logement avec l'identifiant qui se trouve dans l'URL.
 	let housingFiltered = housing.filter((housing) => housing.id === params.housingId);
+	console.log(housingFiltered);
 	let thisHousing = housingFiltered[0];
 
+	console.log(thisHousing);
 	let navigate = useNavigate();
 
 	useEffect(() => {
@@ -38,10 +41,13 @@ function Housing() {
 					<p className="housing-location">{thisHousing.location}</p>
 					<div className="tags-wrapper">
 						{thisHousing.tags.map((tag, index) => (
+							//Utilisation de map() qui permet de prendre une liste de données et de la transformer en liste de composants
+							//Pour chaque entrée du tableau on retourne un élément
 							<Tags tagName={tag} key={`${tag}-${index}`} />
 						))}
 					</div>
 				</div>
+
 				<div className="informations-wrapper">
 					<Collapse title="Description" content={thisHousing.description} />
 					<Collapse title="Équipements" content={thisHousing.equipments} />
